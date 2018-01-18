d70ba11f8296f7fd61684b07b1cce3a628e1760e
Glenn Downing <downing@cs.utexas.edu>
2018-01-17 21:19:33 -0600

    another commit

--- notes/01-17.py
+++ notes/01-17.py
@@ -1,6 +1,30 @@
 # -----------
-# Wec, 17 Jan
+# Wed, 17 Jan
 # -----------
 
 """
+Exercise:
+
+1. exchange your name, cell, and e-mail with another student
+
+2. discuss your expectations of this class
+
+3 min
+
+3. Google "cs373 fall 2017" and look for "CS373 Fall 2017: <name>".
+
+4. pick one of the results and read the last entry which is a summary of that student's experience in this class
+
+5 min
+
+5. exchange your name, cell, and e-mail with a second student
+
+6. discuss your new expectations of this class
+
+3 min
+
+very big disconnect between what we'll do in lecture and what I'll expect you to do
+outside, doing this relatively large Web dev project in a team of 5 or 6
+
+in lecture we'll cover Python, a little JavaScript, relational algegra, SQL, a little Java, and refactoring
 """

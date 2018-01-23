9765b40a010418c7c822dea7d1efb3baeaa23668
Glenn Downing <downing@cs.utexas.edu>
2018-01-22 18:48:32 -0600

    another commit

--- notes/01-22.py
+++ notes/01-22.py
@@ -3,4 +3,47 @@
 # -----------
 
 """
+competitive programming
+
+contests every couple of weeks, GDC 1.304
+CS 104c, every Fri, 2pm, GDC 1.304
+
+ACM is 80 years old
+they've been running a national contest of competitive programming for 40 years
+20 years ago, Unversidad de Valladolid (UVa) created an online judge
+now there are many, many online judges (Codeforces, HackerRank, Kattis, LeetCode, Sphere Online, etc.)
+
+in Oct/Nov we take the best 12 students, 4 teams, to a regional contest, has been at Baylor
+110 regions in the world
+65 teams from 25 schools in 3 states in our region
+
+3 years ago
+came in 2nd, Rice came in 1st, Morocco
+
+2 years ago
+came in 2nd, Rice came in 1st, Thailand
+
+1 year ago
+came in 1st, 2nd, 3rd, 4th, South Dakota
+
+3 mon ago
+came in 1st, Bejing this coming Apr
+"""
+
+"""
+use the GitHub Classroom link to create the repo
+when you're done, give us a Git SHA
+you're going to fill out a JSON form on Canvas with that Git SHA and other details
+"""
+
+"""
+REPL: read, eval, print, loop
+"""
+
+"""
+1. run the code as is, confirm success
+2. fix the tests, maybe write more tests
+3. run the code, confirm failure
+4. fix the code
+5. run the code, confirm success
 """
